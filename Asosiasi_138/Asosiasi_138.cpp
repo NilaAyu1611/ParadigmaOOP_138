327d06704bc8eb0a916fcad5c1ae4be45e055933
NilaAyu1611 <nilaayucw@gmail.com>
2023-05-22 17:26:58 +0700

    6 member (2 member function : constructor dan destructor)

--- Asosiasi_138/Asosiasi_138.cpp
+++ Asosiasi_138/Asosiasi_138.cpp
@@ -7,5 +7,11 @@ class pasien {
 public: 
 	string nama;
 	vector<dokter*> daftar_dokter;
+	pasien(string pNama) : nama(pNama) {
+		cout << "Pasien \"" << nama << "\" ada\n";
+	}
+	~pasien() {
+		cout << "Pasien \"" << nama << "\" tidak ada\n";
+	}
 	
 };
